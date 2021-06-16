d558dddc45bae79e3040a366ef9dbbd8b51fcc64
hainguyen <hainguyen4391@gmail.com>
2021-06-16 21:17:31 +0700

    add dj-database-url

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ecommerce.wsgi.application
+web: gunicorn yourdjangoweb.wsgi --log-file -
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -203,5 +203,9 @@ LOGGING = {
 # CORS Settings
 CORS_ORIGIN_ALLOW_ALL = True
 
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
 import django_heroku
 django_heroku.settings(locals())
