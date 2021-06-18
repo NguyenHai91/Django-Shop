771226f8cbb580e565adfaed4eda0bac3ccc3610
hainguyen <hainguyen4391@gmail.com>
2021-06-18 09:41:24 +0700

    edit setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
