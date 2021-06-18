4075ed93a2991b6c447861cbfd8efb118887ab92
hainguyen <hainguyen4391@gmail.com>
2021-06-18 10:34:22 +0700

    edit setting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['hainguyen.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hainguyen91.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     # in-build apps
